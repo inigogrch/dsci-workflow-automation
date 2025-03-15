71fe8bc996ed529cb4f89801bb6f5f5432a824cd
miketham24 <michaelthamk@gmail.com>
2025-03-15 10:55:41 -0700

    Changes to 02-clean_data.R and makefile

--- analysis/02-clean_data.R
+++ analysis/02-clean_data.R
@@ -17,8 +17,14 @@ Usage:
   02_clean_data.R --input=<input> --output=<output>
 ")
 
-<<<<<<< HEAD
-data <- read.csv(doc$input, col_names = FALSE, na.strings = "?")
+data <- read_csv(doc$input, col_names = FALSE, na.strings = "?")
+
+data <- na.omit(data)
+colnames(data) <- c(
+  "age", "workclass", "fnlwgt", "education", "education_num",
+  "marital_status", "occupation", "relationship", "race", "sex", "capital_gain", # nolint
+  "capital_loss", "hours_per_week", "native_country", "income"
+)
 
 print("Column names in dataset:")
 print(colnames(data))
@@ -27,15 +33,6 @@ if (!"income" %in% colnames(data)) {
   stop("Error: The dataset does not contain an 'income' column.")
 }
 
-=======
-data <- read_csv(doc$input, col_names = FALSE, na.strings = "?")
->>>>>>> c776e4b (final abstractions)
-data <- na.omit(data)
-colnames(data) <- c(
-  "age", "workclass", "fnlwgt", "education", "education_num",
-  "marital_status", "occupation", "relationship", "race", "sex", "capital_gain", # nolint
-  "capital_loss", "hours_per_week", "native_country", "income"
-)
 data$income <- as.factor(data$income)
 
 write.csv(data, doc$output, row.names = FALSE)
