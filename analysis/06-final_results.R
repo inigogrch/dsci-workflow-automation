dd82c72b13cb12e22d3e8b9453ff1c442f232405
miketham24 <michaelthamk@gmail.com>
2025-03-25 14:48:40 -0700

    refactored final_results script

--- analysis/06-final_results.R
+++ analysis/06-final_results.R
@@ -53,28 +53,31 @@ conf_matrix_plot <- ggplot(
   )
 ggsave(doc$output_plot, plot = conf_matrix_plot)
 
-# Metrics Table abstraction
-tp <- confusion_matrix[">50K", ">50K"] # True Positives
-tn <- confusion_matrix["<=50K", "<=50K"] # True Negatives
-fp <- confusion_matrix[">50K", "<=50K"] # False Positives
-fn <- confusion_matrix["<=50K", ">50K"] # False Negatives
-n <- tp + tn + fp + fn # Total observations
-# Sensitivity (SN)
-sensitivity <- tp / (tp + fn)
-# Specificity (SP)
-specificity <- tn / (tn + fp)
-# Precision (PR)
-precision <- tp / (tp + fp)
-# Accuracy (ACC)
-accuracy <- (tp + tn) / n
-# Cohen's Kappa (κ)
-observed_accuracy <- (tp + tn) / n
-expected_accuracy <- ((tp + fp) / n) * ((tp + fn) / n) + ((tn + fp) / n) * ((tn + fn) / n) # nolint
-kappa <- (observed_accuracy - expected_accuracy) / (1 - expected_accuracy)
-metrics_df <- data.frame(
-  Metric = c("Sensitivity", "Specificity", "Precision", "Accuracy", "Cohen's Kappa"), # nolint
-  Value  = c(sensitivity, specificity, precision, accuracy, kappa)
-)
+# # Metrics Table abstraction
+# tp <- confusion_matrix[">50K", ">50K"] # True Positives
+# tn <- confusion_matrix["<=50K", "<=50K"] # True Negatives
+# fp <- confusion_matrix[">50K", "<=50K"] # False Positives
+# fn <- confusion_matrix["<=50K", ">50K"] # False Negatives
+# n <- tp + tn + fp + fn # Total observations
+# # Sensitivity (SN)
+# sensitivity <- tp / (tp + fn)
+# # Specificity (SP)
+# specificity <- tn / (tn + fp)
+# # Precision (PR)
+# precision <- tp / (tp + fp)
+# # Accuracy (ACC)
+# accuracy <- (tp + tn) / n
+# # Cohen's Kappa (κ)
+# observed_accuracy <- (tp + tn) / n
+# expected_accuracy <- ((tp + fp) / n) * ((tp + fn) / n) + ((tn + fp) / n) * ((tn + fn) / n) # nolint
+# kappa <- (observed_accuracy - expected_accuracy) / (1 - expected_accuracy)
+# metrics_df <- data.frame(
+#   Metric = c("Sensitivity", "Specificity", "Precision", "Accuracy", "Cohen's Kappa"), # nolint
+#   Value  = c(sensitivity, specificity, precision, accuracy, kappa)
+# )
+# write_csv(metrics_df, doc$output_table)
+
+metrics_df <- calculate_model_metrics(confusion_matrix)
 write_csv(metrics_df, doc$output_table)
 
 message("Confusion matrix and final results table created successfully.")
