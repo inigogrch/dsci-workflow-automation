2cdc671b76d856d95cb6fa6975ca57152142c67f
Benjamin Gerochi <bgerochi@gmail.com>
2025-04-11 12:32:22 -0700

    03-training_split.R script data validations finalized

--- analysis/03-training_split.R
+++ analysis/03-training_split.R
@@ -18,20 +18,10 @@ library(pointblank)
 # source("R/train_test_split.R")
 library(incomepredictability)
 
-# doc <- docopt("
-# Usage:
-#   analysis/03-training_split.R --input=<input> --output_train=<output> --output_test=<output>
-# ")
-
-doc <- docopt::docopt("
+doc <- docopt("
 Usage:
   analysis/03-training_split.R --input=<input> --output_train=<output> --output_test=<output>
-",
-args = c(
-  "--input", "/Users/benjogerochi/dsci310/dsci-310-group-8/data/clean/adult_clean.csv",
-  "--output_train", "/Users/benjogerochi/dsci310/dsci-310-group-8/data/clean/adult_training.csv",
-  "--output_test", "/Users/benjogerochi/dsci310/dsci-310-group-8/data/clean/adult_testing.csv"
-))
+")
 
 set.seed(1234)
 
@@ -70,16 +60,19 @@ train_agent <- create_agent(
   col_is_character(
     columns = vars(workclass, education, marital_status, occupation, relationship, race, sex, native_country)) %>%
   col_is_factor(
-    columns = vars(income))
-    # No duplicate observations
+    columns = vars(income)) %>%
+  # No duplicate observations
   rows_distinct() %>%
   # No outliers or anomalous values
-  col_vals_between(columns = vars(X1), left = 0, right = 100) %>% # age (X1): 0-100
-  col_vals_between(columns = vars(X3), left = 1, right = Inf) %>%   # fnlwgt (X3): positive
-  col_vals_between(columns = vars(X5), left = 1, right = 16) %>% # education_num (X5): 1-16
-  col_vals_between(columns = vars(X11), left = 0, right = Inf) %>% # capital_gain (X11): 0 or positive
-  col_vals_between(columns = vars(X12), left = 0, right = Inf) %>% # capital_loss (X12): 0 or positive
-  col_vals_between(columns = vars(X13), left = 1, right = 100) %>% # hours_per_week (X13) 1-100
+  col_vals_between(columns = vars(age), left = 0, right = 100) %>% # age (X1): 0-100
+  col_vals_between(columns = vars(fnlwgt), left = 1, right = Inf) %>%   # fnlwgt (X3): positive
+  col_vals_between(columns = vars(education_num), left = 1, right = 16) %>% # education_num (X5): 1-16
+  col_vals_between(columns = vars(capital_gain), left = 0, right = Inf) %>% # capital_gain (X11): 0 or positive
+  col_vals_between(columns = vars(capital_loss), left = 0, right = Inf) %>% # capital_loss (X12): 0 or positive
+  col_vals_between(columns = vars(hours_per_week), left = 1, right = 100) # hours_per_week (X13) 1-100
+
+# We choose to omit category level and value check because we standardize all categorical values in the previous validation and in cleaning
+# Rechecking them on the cleaned data and subsequent splits would be redundant
 # Run the training data validation
 train_agent <- interrogate(train_agent)
 
@@ -108,7 +101,21 @@ test_agent <- create_agent(
   col_is_character(
     columns = vars(workclass, education, marital_status, occupation, relationship, race, sex, native_country)) %>%
   col_is_factor(
-    columns = vars(income))
+    columns = vars(income)) %>%
+  # No duplicate observations
+  rows_distinct() %>%
+  # No outliers or anomalous values
+  col_vals_between(columns = vars(age), left = 0, right = 100) %>% # age (X1): 0-100
+  col_vals_between(columns = vars(fnlwgt), left = 1, right = Inf) %>%   # fnlwgt (X3): positive
+  col_vals_between(columns = vars(education_num), left = 1, right = 16) %>% # education_num (X5): 1-16
+  col_vals_between(columns = vars(capital_gain), left = 0, right = Inf) %>% # capital_gain (X11): 0 or positive
+  col_vals_between(columns = vars(capital_loss), left = 0, right = Inf) %>% # capital_loss (X12): 0 or positive
+  col_vals_between(columns = vars(hours_per_week), left = 1, right = 100) # hours_per_week (X13) 1-100
+
+  # We choose to omit category level and value check because we standardize all categorical values in the previous validation and in cleaning
+  # Rechecking them on the cleaned data and subsequent splits would be redundant
+  # Run the testing data validation
+  test_agent <- interrogate(test_agent)
 
 write.csv(train_data, doc$output_train, row.names = FALSE)
 write.csv(test_data, doc$output_test, row.names = FALSE)
